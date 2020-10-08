b16cb7651b69ac17b2fa4b0342c8a28a628c2dbc
jhon.phileppe.reyes.flores <jhon.reyes@openserver.com.pe>
2020-10-08 17:38:19 -0500

    Implementando cambios finales

--- Program.cs
+++ Program.cs
@@ -15,13 +15,12 @@ namespace CoreEscuela
         {
             //Eventos => Estos pueden ser agregados o elimnados dependiendo del caso
             AppDomain.CurrentDomain.ProcessExit += AccionDelEvento;
-            AppDomain.CurrentDomain.ProcessExit += (o, s) => Printer.Beep(1200, 1000, 1);
+            //AppDomain.CurrentDomain.ProcessExit += (o, s) => Printer.Beep(1200, 1000, 1);
             AppDomain.CurrentDomain.ProcessExit -= AccionDelEvento;
 
             var engine = new EscuelaEngine();
-            engine.Inicializar();
-            Printer.WriteTitle("BIENVENIDOS A LA ESCUELA");
-            //Printer.Beep(10000, cantidad:10);
+            MostrarMenuOpciones(ref engine);
+            /*
             ImpimirCursosEscuela(engine.Escuela);
             var lista = engine.GetObjetoEscuelaBases();
 
@@ -37,27 +36,9 @@ namespace CoreEscuela
             var topPromedios = reporteador.GetTopPromedio(10);
             Printer.WriteTitle("Fin de reportes...");
 
-            Printer.WriteTitle("Captura de una nueva Evaluación por Consola");
-            var newEval = new Evaluacion(); //Nombre y Nota
-            string nombre, nota;
+            IngresarEvaluacion();
+            */
 
-            WriteLine("Ingrese el nombre de la evaluación:");
-            Printer.PresioneEnter();
-            nombre = ReadLine();
-
-            if (string.IsNullOrWhiteSpace(nombre)) throw new ArgumentNullException("El valor del nombre no puede ser null");
-            newEval.Nombre = nombre.ToLower();
-            WriteLine("Nombre de la evaluación ingresado correctamente!");
-
-            WriteLine("Ingrese la nota de la evaluación:");
-            Printer.PresioneEnter();
-            nota = ReadLine();
-
-            if (string.IsNullOrWhiteSpace(nota)) throw new ArgumentNullException("El valor de la nota no puede ser null");
-            if (!float.TryParse(nota, out float minota)) throw new ArgumentException("El valor de la nota debe ser un número float");
-            if (minota < 0 || minota > 5) throw new ArgumentOutOfRangeException("El valor de la nota esta fuera de rango");
-            newEval.Nota = minota;
-            WriteLine("Nota de la evaluación ingresado correctamente!");
             #region Comentarios
             /*
             Dictionary<int,string> diccionario= new Dictionary<int, string>();
@@ -113,14 +94,14 @@ namespace CoreEscuela
             WriteLine($"Alumno: {objDummy.GetType()}");   
 
             var evaluacion = new Evaluacion() {
-                Nombre = "Evaluación de mate!",
+                Nombre = "Evaluacion de mate!",
                 Nota = 4.5f 
             };
-            Printer.WriteTitle("Evaluación");
-            WriteLine($"Evaluación: {evaluacion.Nombre}");
-            WriteLine($"Evaluación: {evaluacion.UniqueId}");
-            WriteLine($"Evaluación: {evaluacion.Nota}");
-            WriteLine($"Evaluación: {evaluacion.GetType()}");  
+            Printer.WriteTitle("Evaluacion");
+            WriteLine($"Evaluacion: {evaluacion.Nombre}");
+            WriteLine($"Evaluacion: {evaluacion.UniqueId}");
+            WriteLine($"Evaluacion: {evaluacion.Nota}");
+            WriteLine($"Evaluacion: {evaluacion.GetType()}");  
 
             //ob = evaluacion;
             Printer.WriteTitle("ObjetoEscuela 1");
@@ -136,7 +117,7 @@ namespace CoreEscuela
             Alumno alumnoRecuperado2 = ob as Alumno; 
             //Cuando utilizamos el [as] considera si es posible convertirlo a "Alumno" retorna el ojeto, caso contrario retorna null
             if(alumnoRecuperado2 != null){
-                //lógica para el caso..
+                //logica para el caso..
             }
             */
             #endregion
@@ -147,7 +128,7 @@ namespace CoreEscuela
             //throw new NotImplementedException();
             Printer.WriteTitle("Saliendo");
             Printer.Beep(3000, 1000, 3);
-            Printer.WriteTitle("SALIÓ");
+            Printer.WriteTitle("SALIo");
         }
 
         private static void ImpimirCursosEscuela(Escuela escuela)
@@ -161,6 +142,180 @@ namespace CoreEscuela
                 {
                     WriteLine($"Nombre {curso.Nombre  }, Id  {curso.UniqueId}");
                 }
+            }
+        }
+
+        private static void IngresarEvaluacion()
+        {
+            Printer.WriteTitle("Captura de una nueva Evaluacion por Consola");
+            var newEval = new Evaluacion(); //Nombre y Nota
+            string nombre, nota;
+
+            WriteLine("Ingrese el nombre de la evaluacion:");
+            Printer.PresioneEnter();
+            nombre = ReadLine();
+
+            if (string.IsNullOrWhiteSpace(nombre)) throw new ArgumentNullException("El valor del nombre no puede ser null");
+            newEval.Nombre = nombre.ToLower();
+            WriteLine("Nombre de la evaluacion ingresado correctamente!");
+
+            WriteLine("Ingrese la nota de la evaluacion:");
+            Printer.PresioneEnter();
+            nota = ReadLine();
+
+            if (string.IsNullOrWhiteSpace(nota)) throw new ArgumentNullException("El valor de la nota no puede ser null");
+            if (!float.TryParse(nota, out float minota)) throw new ArgumentException("El valor de la nota debe ser un numero float");
+            if (minota < 0 || minota > 5) throw new ArgumentOutOfRangeException("El valor de la nota esta fuera de rango");
+            newEval.Nota = minota;
+            WriteLine("Nota de la evaluacion ingresado correctamente!");
+        }
+
+        public static void MostrarMenuOpciones(ref EscuelaEngine escuela)
+        {
+            Printer.WriteTitle("BIENVENIDOS A LA ESCUELA");
+            string opcion = string.Empty; bool mostrarMenu = true;
+            while (opcion != "9")
+            {
+                if (mostrarMenu)
+                {
+                    WriteLine("1. Generar datos");
+                    WriteLine("2. Imprimir cursos escuela");
+                    WriteLine("3. Imprimir diccionario escuela");
+                    WriteLine("4. Ver reporte Evaluaciones");
+                    WriteLine("5. Ver reporte Asignaturas");
+                    WriteLine("6. Ver reporte Evaluaciones por Asignaturas");
+                    WriteLine("7. Ver Promedios por Alumnos por Asignatura");
+                    WriteLine("8. Ver {x} mejores promedios");
+                    WriteLine("9. Salir del programa");
+
+                    WriteLine("Ingrese una de las opciones mostradas y presione ENTER...");
+                }
+                opcion = ReadLine();
+                EvaluarOpcionIngresada(opcion, ref escuela, out mostrarMenu);
+            }
+        }
+
+        public static bool ValidarDatosGenerados(EscuelaEngine escuela, out bool mostrarmnu)
+        {
+            mostrarmnu = true;
+            if (escuela?.Escuela is null)
+            {
+                mostrarmnu = false;
+                WriteLine("Aun no existen datos generados en la escuela.");
+                return false;
+            }
+            return true;
+        }
+
+        public static void EvaluarOpcionIngresada(string opcion, ref EscuelaEngine escuela, out bool mostrarmnu)
+        {
+            var dicObjetos = new Dictionary<LlaveDiccionario, IEnumerable<ObjetoEscuelaBase>>();
+            Reporteador reporteador;
+            mostrarmnu = true;
+            switch (opcion.ToUpper())
+            {
+                case "1": //Generar datos
+                    WriteLine("\nGenerando informacion...");
+                    escuela.Inicializar();
+                    WriteLine("Informacion generada con exito\n");
+                    break;
+
+                case "2": //Imprimir cursos escuela
+                    if (!ValidarDatosGenerados(escuela, out mostrarmnu)) return;
+
+                    WriteLine("\nImprimiendo informacion...");
+                    ImpimirCursosEscuela(escuela.Escuela);
+                    WriteLine("Impresion generada con exito\n");
+                    break;
+
+                case "3": //Imprimir diccionario escuela
+                    if (!ValidarDatosGenerados(escuela, out mostrarmnu)) return;
+
+                    WriteLine("\nImprimiendo informacion...");
+                    dicObjetos = escuela.GetDiccionarioObjetos();
+                    escuela.ImprimirDiccionario(dicObjetos, true);
+                    WriteLine("Impresion generada con exito\n");
+
+                    break;
+
+                case "4": //Ver reporte evaluaciones
+                    if (!ValidarDatosGenerados(escuela, out mostrarmnu)) return;
+
+                    WriteLine("\nImprimiendo informacion...");
+                    dicObjetos = escuela.GetDiccionarioObjetos();
+                    reporteador = new Reporteador(dicObjetos);
+                    var evaluaciones = reporteador.GetListaEvaluaciones();
+                    Printer.GenerateTable(new int[] { 35, 55, 15 }, new string[] { "Alumno", "Asignatura", "Nota" }, evaluaciones);
+
+                    WriteLine("Impresion generada con exito\n");
+
+                    break;
+
+                case "5": //Ver reporte de asignaturas
+                    if (!ValidarDatosGenerados(escuela, out mostrarmnu)) return;
+
+                    WriteLine("\nImprimiendo informacion...");
+                    dicObjetos = escuela.GetDiccionarioObjetos();
+                    reporteador = new Reporteador(dicObjetos);
+                    var asignaturas = reporteador.GetListaAsignaturas();
+                    Printer.GenerateTable(new int[] { 25 }, new string[] { "Asignatura" }, asignaturas);
+
+                    WriteLine("Impresion generada con exito\n");
+
+                    break;
+
+                case "6": //Ver reporte Evaluaciones por asignatura
+                    if (!ValidarDatosGenerados(escuela, out mostrarmnu)) return;
+
+                    WriteLine("\nImprimiendo informacion...");
+                    dicObjetos = escuela.GetDiccionarioObjetos();
+                    reporteador = new Reporteador(dicObjetos);
+                    var listaEvalXAsig = reporteador.GetDicEvaluacionesxAsignatura();
+                    Printer.GenerateTable(new int[] { 55, 15 }, new string[] { "Asignatura/Nombre", "Nota" }, listaEvalXAsig);
+
+                    WriteLine("Impresion generada con exito\n");
+
+                    break;
+
+                case "7": //Ver promedios por alumno y asignatura
+                    if (!ValidarDatosGenerados(escuela, out mostrarmnu)) return;
+
+                    WriteLine("\nImprimiendo informacion...");
+                    dicObjetos = escuela.GetDiccionarioObjetos();
+                    reporteador = new Reporteador(dicObjetos);
+                    var promedios = reporteador.GetPromediosAlumnosxAsignatura();
+                    Printer.GenerateTable(new int[] { 55, 15 }, new string[] { "Alumno", "Promedio" }, promedios);
+
+                    WriteLine("Impresion generada con exito\n");
+
+                    break;
+
+                case "8": //Ver {x} mejores proyectos
+                    if (!ValidarDatosGenerados(escuela, out mostrarmnu)) return;
+
+
+                    WriteLine("\nIngreses la cantidad de promedios a mostrar...");
+                    string cantidad = ReadLine();
+                    if (!int.TryParse(cantidad, out int top)) new Exception("Error al digitar la cantida...");
+
+                    dicObjetos = escuela.GetDiccionarioObjetos();
+                    reporteador = new Reporteador(dicObjetos);
+
+                    var topPromedios = reporteador.GetTopPromedio(top);
+                    Printer.GenerateTable(new int[] { 25, 5, 55, 15 }, new string[] { "Asignatura", "Item", "Alumno", "Promedio" }, topPromedios);
+
+                    WriteLine("Impresion generada con exito\n");
+
+                    break;
+
+                case "9":
+                    mostrarmnu = false;
+                    break;
+
+                default:
+                    mostrarmnu = false;
+                    WriteLine("La opcion ingresada no es valida. Por favor reintente ingresando el numero correcto.");
+                    break;
             }
         }
     }
--- Util/Printer.cs
+++ Util/Printer.cs
@@ -1,3 +1,7 @@
+using System.Collections.Generic;
+using System.Linq;
+using CoreEscuela.Entidades;
+using Etapa5.Entidades;
 using static System.Console;
 
 namespace CoreEscuela.Util
@@ -16,18 +20,133 @@ namespace CoreEscuela.Util
 
         public static void WriteTitle(string titulo)
         {
-            var tamaño =titulo.Length + 4;
+            var tamaño = titulo.Length + 4;
             DrawLine(tamaño);
             WriteLine($"| {titulo} |");
             DrawLine(tamaño);
         }
 
-        public static void Beep(int hz = 2000, int tiempo=500, int cantidad =1)
+        public static void Beep(int hz = 2000, int tiempo = 500, int cantidad = 1)
         {
             while (cantidad-- > 0)
             {
-                System.Console.Beep(hz, tiempo);
+                Beep(hz, tiempo);
             }
+        }
+
+        public static void GenerateTable(int[] spaceWithColumns, string[] columnsName)
+        {
+            //Imprimiendo las cabeceras
+            if (spaceWithColumns?.Count() == 0) return;
+            string linea = "|" + string.Concat(Enumerable.Repeat("*", spaceWithColumns.Sum() + (columnsName.Length - 1))) + "|";
+            WriteLine($"{linea}");
+
+            string title = "|"; int index = 0;
+            foreach (var item in columnsName)
+            {
+                var name = item.Trim();
+                if (spaceWithColumns[index] < name.Length) throw new System.Exception($"La longitud de la columna {item} excede en su tamaño.");
+                var newtitulo = name.PadRight(spaceWithColumns[index]) + "|";
+
+                title += newtitulo;
+                index += 1;
+            }
+
+            WriteLine($"{title}");
+            WriteLine($"{linea}");
+        }
+
+        public static void GenerateTable(int[] spaceWithColumns, string[] columnsName, IEnumerable<ObjetoEscuelaBase> data)
+        {
+            //Imprimiendo cabeceras
+            GenerateTable(spaceWithColumns, columnsName);
+            string linea = "|" + string.Concat(Enumerable.Repeat("*", spaceWithColumns.Sum() + (columnsName.Length - 1))) + "|";
+
+            //Imprimiendo los datos
+            foreach (var item in data)
+            {
+                var dataLine = "";
+                switch (item.GetType().Name.ToUpper())
+                {
+                    case "EVALUACION":
+                        var eval = item as Evaluacion;
+                        dataLine = "|" + eval.Alumno.Nombre.PadRight(spaceWithColumns[0]) + "|" + eval.Asignatura.Nombre.PadRight(spaceWithColumns[1]) + "|" + eval.Nota.ToString().PadRight(spaceWithColumns[2]) + "|";
+                        break;
+                    default:
+                        break;
+                }
+                WriteLine($"{dataLine}");
+            }
+            WriteLine($"{linea}");
+        }
+
+        public static void GenerateTable(int[] spaceWithColumns, string[] columnsName, IEnumerable<string> data)
+        {
+            //Imprimiendo cabeceras
+            GenerateTable(spaceWithColumns, columnsName);
+            string linea = "|" + string.Concat(Enumerable.Repeat("*", spaceWithColumns.Sum() + (columnsName.Length - 1))) + "|";
+
+            //Imprimiendo los datos
+            foreach (var item in data)
+            {
+                var dataLine = "|" + item.PadRight(spaceWithColumns[0]) + "|";
+
+                WriteLine($"{dataLine}");
+            }
+            WriteLine($"{linea}");
+        }
+
+        public static void GenerateTable(int[] spaceWithColumns, string[] columnsName, Dictionary<string, IEnumerable<Evaluacion>> data)
+        {
+            //Imprimiendo cabeceras
+            GenerateTable(spaceWithColumns, columnsName);
+            var spaceTotal = spaceWithColumns.Sum() + (columnsName.Length - 1);
+            string linea = "|" + string.Concat(Enumerable.Repeat("*", spaceTotal)) + "|";
+            bool firstTitle = true;
+
+            //Imprimiendo los datos
+            foreach (var asignatura in data)
+            {
+                if (!firstTitle) WriteLine($"{linea}");
+                var dataLine = "|" + asignatura.Key.PadRight(spaceTotal) + "|";
+                WriteLine($"{dataLine}");
+                WriteLine($"{linea}");
+
+                var evaluaciones = data.Values.ToList();
+                foreach (var item2 in evaluaciones)
+                {
+                    foreach (var item3 in item2)
+                    {
+                        if (item3.Asignatura.Nombre == asignatura.Key)
+                        {
+                            dataLine = "|" + item3.Alumno.Nombre.PadRight(spaceWithColumns[0]) + "|" + item3.Nota.ToString().PadRight(spaceWithColumns[1]) + "|";
+                            WriteLine($"{dataLine}");
+                        }
+                    }
+                    firstTitle = false;
+                }
+            }
+            WriteLine($"{linea}");
+        }
+
+        public static void GenerateTable(int[] spaceWithColumns, string[] columnsName, Dictionary<string, IEnumerable<AlumnoPromedio>> data)
+        {
+            //Imprimiendo cabeceras
+            GenerateTable(spaceWithColumns, columnsName);
+            string linea = "|" + string.Concat(Enumerable.Repeat("*", spaceWithColumns.Sum() + (columnsName.Length - 1))) + "|";
+
+            //Imprimiendo los datos
+            foreach (var item in data)
+            {
+                var position = 1;
+                foreach (var item2 in item.Value)
+                {
+                    var dataLine = "|" + item.Key.PadRight(spaceWithColumns[0]) + "|" + position.ToString().PadLeft(spaceWithColumns[1]) + "|" + item2.AlumnoNombre.PadRight(spaceWithColumns[2]) + "|" + item2.Promedio.ToString().PadRight(spaceWithColumns[3]) + "|";
+                    WriteLine($"{dataLine}");
+                    position += 1;
+                }
+            }
+            WriteLine($"{linea}");
         }
     }
 }
